ba9c0b55fafde1072c4e9406f1dfc112fe891295
Moling <1970115881@qq.com>
2024-03-09 01:37:40 +0800

    Update pyEnv.py

--- utils/pyEnv.py
+++ utils/pyEnv.py
@@ -1,45 +1,46 @@
 import os
-import re
 from datetime import datetime, timedelta
 import urllib.parse
 
+# 定义全局变量
+environmentVariable = []
 
-environmentVariable = [
-    "",
-    "",
-]
+
+def custom_print(*args, **kwargs):
+    """自定义打印函数，根据DEBUG环境变量决定是否打印"""
+    if os.getenv("DEBUG") != "false":
+        print(*args, **kwargs)
 
 
 def get_env(envName: str) -> list:
+    global environmentVariable
     envName = envName.upper()
     if envName in os.environ:
+        custom_print(
+            f"检测到环境变量{envName}存在，开始解析环境变量"
+        )  # 使用自定义的打印函数
         if "&" in os.environ[envName]:
             environmentVariable = os.environ[envName].split("&")
         elif "\n" in os.environ[envName]:
             environmentVariable = os.environ[envName].split("\n")
+        else:
+            environmentVariable = [os.environ.get(envName, "")]
     else:
-        environmentVariable = [os.environ[envName]]
+        environmentVariable = []
 
     if any("GITHUB" in key for key in os.environ):
-        print(
-            "请勿使用github action运行此脚本,无论你是从你自己的私库还是其他哪里拉取的源代码，都会导致我被封号\n"
-        )
+        custom_print("请勿使用github action运行此脚本...")
 
     # 过滤空字符串并去重
     environmentVariable = list(set(filter(None, environmentVariable)))
 
-    if os.getenv("DEBUG") == "false":
-        # 重定义print函数以禁用打印
-        def print(*args, **kwargs):
-            pass
-
-    print(
+    custom_print(
         f"\n====================共{len(environmentVariable)}个任务=================\n"
     )
 
     # 调整为当前时区的时间
     current_time = datetime.now() + timedelta(hours=8)
-    print(
+    custom_print(
         f"============脚本执行时间：{current_time.strftime('%Y-%m-%d %H:%M:%S')}=============\n"
     )
 
