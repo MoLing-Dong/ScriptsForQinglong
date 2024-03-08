5e3a5ab88f8766933def73b74d36482a1f9c67a3
Moling <1970115881@qq.com>
2024-03-09 01:12:31 +0800

    Update Env.js

--- utils/Env.js
+++ utils/Env.js
@@ -22,7 +22,8 @@ module.exports = function getEnv(envName) {
       environmentVariable = [process.env.envName];
     }
   } else {
-    console.log(`\n环境变量：${process.env}\n`);
+    // 打印所有环境变量
+    console.log(`\n环境变量：${JSON.stringify(process.env)}\n`);
   }
   if (JSON.stringify(process.env).indexOf("GITHUB") > -1) {
     console.log(
