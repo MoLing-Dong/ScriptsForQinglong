018ea07729c731231e0ff640157b4ca1b5485912
Moling <1970115881@qq.com>
2024-03-08 21:37:13 +0800

    0

--- main.js
+++
@@ -1,5 +0,0 @@
-import path from "node:path";
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-
-console.log(path.resolve(dirname(fileURLToPath(import.meta.url)), "dist"));
