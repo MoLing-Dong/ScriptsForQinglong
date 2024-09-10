1618f60e67178e92bb10a4c9639180b1048fcc08
Moling <1970115881@qq.com>
2024-09-10 19:02:58 +0800

    refactor(utils): 更新环境变量获取逻辑及添加IP获取功能
    重构环境变量获取方法，支持中文字符编码，并添加获取当前公网IP的功能。同时，引入日志记录，增强脚本执行情况的可追踪性。

--- utils/pyEnv.py
+++ utils/pyEnv.py
@@ -1,48 +1,82 @@
+from loguru import logger
 import os
-from datetime import datetime, timedelta
 import urllib.parse
-
-# 定义全局变量
-environmentVariable = []
-
-
-def custom_print(*args, **kwargs):
-    """自定义打印函数，根据DEBUG环境变量决定是否打印"""
-    if os.getenv("DEBUG") != "false":
-        print(*args, **kwargs)
+import re
+from datetime import datetime
+import requests
 
 
-def get_env(envName: str) -> list:
-    global environmentVariable
-    envName = envName.upper()
-    if envName in os.environ:
-        custom_print(
-            f"检测到环境变量{envName}存在，开始解析环境变量"
-        )  # 使用自定义的打印函数
-        if "&" in os.environ[envName]:
-            environmentVariable = os.environ[envName].split("&")
-        elif "\n" in os.environ[envName]:
-            environmentVariable = os.environ[envName].split("\n")
+def get_env(env_name):
+    """
+    @Created by Mol on 2024/02/27
+    @description 获取环境变量
+    @param {String} env_name 环境变量名称
+    """
+    env_name = env_name.upper().strip()
+
+    environment_variable = ["", ""]  # 初始化空列表
+    IP = ""
+
+    if os.getenv(env_name):
+        if "&" in os.getenv(env_name):
+            environment_variable = os.getenv(env_name).split("&")
+        elif "\n" in os.getenv(env_name):
+            environment_variable = os.getenv(env_name).split("\n")
         else:
-            environmentVariable = [os.environ.get(envName, "")]
-    else:
-        environmentVariable = []
+            environment_variable = [os.getenv(env_name)]
 
-    if any("GITHUB" in key for key in os.environ):
-        custom_print("请勿使用github action运行此脚本...")
+    if "GITHUB" in str(os.environ):
+        logger.warning(
+            "请勿使用 GitHub Action 运行此脚本, 无论你是从你自己的私库还是其他哪里拉取的源代码，都会导致我被封号\n"
+        )
 
-    # 过滤空字符串并去重
-    environmentVariable = list(set(filter(None, environmentVariable)))
-
-    custom_print(
-        f"\n====================共{len(environmentVariable)}个任务=================\n"
+    # 去重并过滤空值
+    environment_variable = list(
+        set(filter(lambda item: bool(item), environment_variable))
     )
 
-    # 调整为当前时区的时间
-    current_time = datetime.now() + timedelta(hours=8)
-    custom_print(
-        f"============脚本执行时间：{current_time.strftime('%Y-%m-%d %H:%M:%S')}=============\n"
+    if os.getenv("DEBUG") == "false":
+        logger.disable("my_module")
+
+    logger.info(
+        f"\n====================共{len(environment_variable)}个任务=================\n"
+    )
+    logger.info(
+        f"============脚本执行时间：{format_date(datetime.utcnow())}=============\n"
     )
 
-    
-    return environmentVariable
+    # 对环境变量中包含的中文字符进行编码
+    environment_variable = [urllib.parse.quote(item) for item in environment_variable]
+
+    return environment_variable
+
+
+def get_ip():
+    url = "https://www.cip.cc/"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, timeout=5)
+        ip = re.search(
+            r"((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}",
+            response.text,
+        )
+        if ip:
+            logger.info(f"\n当前公网IP: {ip.group(0)}")
+        return ip.group(0) if ip else None
+    except requests.RequestException as e:
+        logger.error(f"获取IP失败: {e}")
+        return None
+
+
+def get_current_activity_script_file_name():
+    import sys
+    import os
+
+    return os.path.basename(sys.argv[0])
+
+
+def format_date(date):
+    return date.strftime("%Y/%m/%d %H:%M:%S")
